e4d89358202c78ed55c43d21257fe04fcf6d738f
Tanner W Stokes <TANNERS@clemson.edu>
2021-03-31 13:33:04 -0400

    Cleanup.

--- SwiftGFXWrapperExample/SwiftGFXWrapperExample/View Models/BoardViewModel.swift
+++ SwiftGFXWrapperExample/SwiftGFXWrapperExample/View Models/BoardViewModel.swift
@@ -6,12 +6,38 @@ import SwiftGFXWrapper
 class ExampleMatrix: GFXMatrix {
     var buffer = Set<Pixel>()
     
+    override init(rows: Int, cols: Int) {
+        super.init(rows: rows, cols: cols)
+        
+        // a buffer to track our pixel state
+        for r in 0..<rows {
+            for c in 0..<cols {
+                buffer.insert(Pixel(x: c, y: r))
+            }
+        }
+        
+        // set the function that's fired on every pixel draw
+        setDrawCallback { [self] x, y, color in
+            guard let dot = buffer.first(
+                    where: { $0.x == x && $0.y == y && $0.color != color }
+            ) else {
+                return
+            }
+           
+            dot.color = color
+        }
+    }
+    
     override func fillScreen(_ c: Int32) {
         // a performance optimized version of fillScreen
         // for our application
         buffer
             .filter { $0.color != Int(c) }
             .forEach { $0.color = Int(c) }
+    }
+    
+    func getPixelAt(row: Int, col: Int) -> Pixel? {
+        buffer.first(where: { $0.x == col && $0.y == row })
     }
 }
 
@@ -29,25 +55,7 @@ class BoardViewModel {
     init(rows: Int, cols: Int) {
         // create a new matrix
         matrix = ExampleMatrix(rows: rows, cols: cols)
-        
-        // a buffer to track our pixel state
-        for r in 0..<rows {
-            for c in 0..<cols {
-                matrix.buffer.insert(Pixel(x: c, y: r))
-            }
-        }
-        
-        // set the function that's fired on every pixel draw
-        matrix.setDrawCallback { [self] x, y, color in
-            guard let dot = matrix.buffer.first(
-                    where: { $0.x == x && $0.y == y && $0.color != color }
-            ) else {
-                return
-            }
-           
-            dot.color = color
-        }
-        
+                
         // use a frame driver to handle the timing of each frame
         let driver = DisplayLinkDriver()
         // tell the driver what function to call on every frame step
@@ -64,7 +72,7 @@ class BoardViewModel {
     }
     
     func getPixelAt(row: Int, col: Int) -> Pixel? {
-        return matrix.buffer.first(where: { $0.x == col && $0.y == row })
+        return matrix.getPixelAt(row: row, col: col)
     }
     
     private func bouncyBox() {
