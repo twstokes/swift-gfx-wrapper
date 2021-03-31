caf9d796c5b4b6670291bb8a5298d47122e38da4
Tanner W Stokes <TANNERS@clemson.edu>
2021-03-31 13:34:44 -0400

    More cleanup.

--- SwiftGFXWrapperExample/SwiftGFXWrapperExample/View Models/BoardViewModel.swift
+++ SwiftGFXWrapperExample/SwiftGFXWrapperExample/View Models/BoardViewModel.swift
@@ -3,44 +3,6 @@ import UIKit
 
 import SwiftGFXWrapper
 
-class ExampleMatrix: GFXMatrix {
-    var buffer = Set<Pixel>()
-    
-    override init(rows: Int, cols: Int) {
-        super.init(rows: rows, cols: cols)
-        
-        // a buffer to track our pixel state
-        for r in 0..<rows {
-            for c in 0..<cols {
-                buffer.insert(Pixel(x: c, y: r))
-            }
-        }
-        
-        // set the function that's fired on every pixel draw
-        setDrawCallback { [self] x, y, color in
-            guard let dot = buffer.first(
-                    where: { $0.x == x && $0.y == y && $0.color != color }
-            ) else {
-                return
-            }
-           
-            dot.color = color
-        }
-    }
-    
-    override func fillScreen(_ c: Int32) {
-        // a performance optimized version of fillScreen
-        // for our application
-        buffer
-            .filter { $0.color != Int(c) }
-            .forEach { $0.color = Int(c) }
-    }
-    
-    func getPixelAt(row: Int, col: Int) -> Pixel? {
-        buffer.first(where: { $0.x == col && $0.y == row })
-    }
-}
-
 class BoardViewModel {
     private let matrix: ExampleMatrix
         
